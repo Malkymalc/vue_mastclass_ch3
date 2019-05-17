2f271657be9fe618a5085e4f2c05ae2ee4a03a24
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2019-05-17 12:19:23 +0100

    Chapter 3 of Masterclass completed.

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+  <h1>Page Not found</h1>
+  <span>
+    Try going to our
+    <router-link :to="{ name: 'PageHome'}"> Home </router-link>
+    Page.
+  </span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PageHome from '@/pages/PageHome'
+import PageThreadShow from '@/pages/PageThreadShow'
+import PageNotFound from '@/pages/PageNotFound'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'PageHome',
+      component: PageHome
+    },
+    {
+      path: '/thread/:id',
+      name: 'PageThreadShow',
+      component: PageThreadShow,
+      props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: PageNotFound
+    }
+  ],
+  mode: 'history'
+})
